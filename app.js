4370902ed90a08ce76ea7554e5a41d9527032e3f
mujeebcodes <mailmujeeb@icloud.com>
2024-01-09 21:49:56 +0000

    nodemailer and node-schedule used to send emails and create cron job respectively

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 const UserModel = require("./model/user");
 const schedule = require("node-schedule");
 const nodemailer = require("nodemailer");
+const db = require("./db");
+
+// connectdb
+db.connect();
 
 app.use(express.static("public"));
 app.use(express.json());
@@ -14,6 +18,7 @@ app.get("/", (req, res) => {
 
 app.post("/add-birthday", async (req, res) => {
   //   const { username, email, dob } = req.body;
+  console.log(req.body);
   try {
     await UserModel.create(req.body);
     res.status(201).json({ msg: "user created successfully" });
@@ -22,7 +27,57 @@ app.post("/add-birthday", async (req, res) => {
   }
 });
 
-schedule;
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.MY_EMAIL,
+    pass: process.env.MY_EMAIL_APP_PASSWORD,
+  },
+});
+
+transporter.verify((error) => {
+  if (error) {
+    console.log(`Error connecting to your email: ${error}`);
+  }
+});
+
+// cron scheduler
+schedule.scheduleJob("0 7 * * *", async () => {
+  try {
+    const today = new Date();
+    const todayMonthDay = {
+      $expr: {
+        $eq: [{ $month: "$dob" }, today.getMonth() + 1],
+      },
+    };
+
+    const todayDayOfMonth = {
+      $expr: {
+        $eq: [{ $dayOfMonth: "$dob" }, today.getDate()],
+      },
+    };
+
+    const birthdays = await UserModel.find({
+      $and: [todayMonthDay, todayDayOfMonth],
+    });
+
+    if (birthdays.length > 0) {
+      birthdays.forEach(async (birthday) => {
+        const mailOptions = {
+          from: process.env.MY_EMAIL,
+          to: birthday.email,
+          subject: `Happy Birthday, ${birthday.username}!`,
+          text: "Wishing you a fantastic birthday!",
+        };
+
+        await transporter.sendMail(mailOptions);
+        console.log(`Birthday email sent to ${birthday.username}`);
+      });
+    }
+  } catch (error) {
+    console.error("Error sending birthday emails:", error);
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
